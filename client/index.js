935b09c66c8d3421bab61edf3caf41fc3d0c477e
CathalRyan96 <cathalryan48@gmail.com>
2018-11-06 10:52:24 +0000

    tidied up folders

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "../src/serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
+
+ReactDOM.render(<App />, document.getElementById("root"));
